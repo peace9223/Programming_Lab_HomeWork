8f6c402963f5875c65ba61da4b176eb2b0c232b7
peace9223 <jhw0404052@gmail.com>
2026-03-31 14:44:01 +0900

    Add files via upload

---
+++ 5week/7.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int t = 0;
+	FILE* in = fopen("input3.txt", "r");
+	FILE* out = fopen("output3.txt", "w");
+	if (in == NULL || out == NULL)
+	{
+		printf("파일을 못염\n");
+		return;
+	}
+	fscanf(in,"%d", &t);
+	for (int i = 0; i < t; i++)
+	{
+		int k = 0;
+		fscanf(in,"%d", &k);
+		int* p = (int*)malloc(k * sizeof(int));
+		int sum = 0, max = 0, min = 100;
+		for (int m = 0; m < k; m++)
+		{
+			fscanf(in,"%d", &p[m]);
+			sum += p[m];
+			if (max < p[m]) max = p[m];
+			if (min > p[m]) min = p[m];
+		}
+		fprintf(out,"%d %d %d\n", sum, max, min);
+		free(p);
+	}
+
+}
