e202eb6ff4f7b935527f03b9b45c81b354e2ae85
peace9223 <jhw0404052@gmail.com>
2026-04-07 14:33:29 +0900

    근사치 값 계산

---
+++ 5week/5-3근사치계산.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+#define ABS(a) (a>0 ? a: -a)
+double myexp(int x)
+{
+	int i;
+	double term, sum, oldsum;
+	oldsum = 0.0;
+	sum = 1.0;
+	term = 1.0;
+	for (i = 1;ABS(sum-oldsum) > 0.0000001; i++)
+	{
+		term *= (double)x / i;
+		oldsum = sum;
+		sum += term;
+	}
+	return sum;
+}
+
+void main()
+{
+	int i;
+	printf("  exp() : ");
+	for (i = 1; i <= 5; i++)
+		printf("%lf ", exp((double)i));
+	printf("\n");
+	printf("myexp() : ");
+	for (i = 1; i <= 5; i++)
+		printf("%lf ", myexp(i));
+	printf("\n");
+}
